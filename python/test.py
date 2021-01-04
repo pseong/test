bf0ab3d83152dc6cb7c3fea5772b385299292080
Guest <guest-ohaqrg@h30249.sgb.skir.kr>
2021-01-04 18:45:05 +0900

    commit

--- python/test.py
+++ python/test.py
@@ -1,12 +1,28 @@
-n, k = map(int, input().split())
+# n = number of types of tiles
+# m = size of the square to make
 
-result = 0
-while n > 1:
-    if n % k == 0:
-        n /= k
-        result += 1
+# 2n lines of the test case = descriptions of tiles types
+#
+
+0 1 2 3
+1 0 5 9
+2 5 0 7
+3 9 7 0
+
+
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    result = False
+    for _ in range(n):
+        t1, t2 = map(int, input().split())
+        t3, t4 = map(int, input().split())
+        if t2 == t3:
+            result = True
+        if m % 2 == 1:
+            result = False
+    if result == True:
+        print("YES")
     else:
-        n -= 1
-        result += 1
-
-print(result)
+        print("NO")
