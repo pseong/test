d4f294ae5f071ada3839fa088a5894d1defce6f0
Guest <guest-n1plcz@h29497.sgb.skir.kr>
2021-01-05 18:05:03 +0900

    commit

--- python/test.py
+++ python/test.py
@@ -1,28 +1,9 @@
-# n = number of types of tiles
-# m = size of the square to make
-
-# 2n lines of the test case = descriptions of tiles types
-#
-
-0 1 2 3
-1 0 5 9
-2 5 0 7
-3 9 7 0
-
+import math
 
 t = int(input())
 
 for _ in range(t):
-    n, m = map(int, input().split())
-    result = False
-    for _ in range(n):
-        t1, t2 = map(int, input().split())
-        t3, t4 = map(int, input().split())
-        if t2 == t3:
-            result = True
-        if m % 2 == 1:
-            result = False
-    if result == True:
-        print("YES")
-    else:
-        print("NO")
+    n = int(input())
+    a = int(math.sqrt(n))
+    b = math.ceil(n / a)
+    print(a + b - 2)
