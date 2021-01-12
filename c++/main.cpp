4caa8f900ac2a3c22f8792992f14557e27180ae4
Guest <guest-sduhup@h29048.sgb.skir.kr>
2021-01-12 19:12:32 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+const int N = 3000010;
+int a[N];
+
 int main() {
     ios_base :: sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        bool skill = false;
+        for(int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+    }
 }
