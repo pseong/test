6b68eff43e4ccb534a52c9faa118dde0f58d33da
Guest <guest-sduhup@h29048.sgb.skir.kr>
2021-01-12 18:12:55 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,19 +1,9 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-            tree_order_statistics_node_update> indexed_set;
-int main()
-{
-    indexed_set s;
-    s.insert(7);
-    s.insert(9);
-    s.insert(2);
-    s.insert(3);
-
-    cout << s.order_of_key(8) << "\n";
-    return 0;
+int main() {
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
 }
