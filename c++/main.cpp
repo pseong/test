49b8ae91ce863cb98fa7a0279750805a10f13f8e
Guest <guest-3cpjpa@h29033.sgb.skir.kr>
2021-01-15 15:47:47 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,69 +2,20 @@
 
 using namespace std;
 
-int cng = 100010;
-void find(vector<int> v, int lv) {
-    if(v.size() < 2 || lv >= cng) return;
-
-    int pre = 0;
-    for(int i : v) {
-        if (pre > i) {
-            cng = min(cng, lv);
-            return;
-        }
-        pre = i;
-    }
-
-    for(auto i = 0; i < v.size() - 1; i++) {
-        int a = v[i];
-        int b = v[i + 1];
-        int f = a^b;
-
-        v[i] = f;
-        v.erase(v.begin() + i + 1);
-
-        int pre = 0;
-        for(int i : v) {
-            if (pre > i) {
-                cng = min(cng, lv + 1);
-                return;
-            }
-            pre = i;
-        }
-
-        v[i] = a;
-        v.insert(v.begin() + i + 1, b);
-    }
-
-    for(auto i = 0; i < v.size() - 1; i++) {
-        int a = v[i];
-        int b = v[i + 1];
-        int f = a^b;
-
-        v[i] = f;
-        v.erase(v.begin() + i + 1);
-
-        find(v, lv + 1);
-
-        v[i] = a;
-        v.insert(v.begin() + i + 1, b);
-    }
-}
-
 int main() {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
 
-    int t;
-    cin >> t;
-    vector<int> v;
-    while(t--) {
-        int a;
-        cin >> a;
-        v.push_back(a);
-    }
-    find(v, 0);
+    int n;
+    cin >> n;
 
-    cout << ((cng == 100010) ? -1 : cng);
+    int dp[1011]{0};
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+    }
+
+    cout << dp[n];
 }
