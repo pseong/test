2cd008c56595d785f768fe343ace4083ac2caa9a
Guest <guest-go2ohp@h29038.sgb.skir.kr>
2021-01-17 20:03:05 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+char rots(char c) {
+    c += 13;
+    if(c > 122) c = (c - 122) + 96;
+}
+
+char rotb(char c) {
+    c += 13;
+    if(c > 90) c = (c - 90) + 64;
+}
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); 
@@ -10,9 +20,13 @@ int main() {
     string s;
     cin >> s;
 
-    int dp[5010]{ 0 };
-    dp[0] = 1;
-    for(int i = 1; i < s.size(); i++) {
-        dp[i] +=-
+    char c[110]{ 0 };
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] >= 65 && s[i] <= 90) c[i] = rotb(s[i]);
+        else if(s[i] >= 97 && s[i] <= 122) c[i] = rots(s[i]);
+        else c[i] = s[i];
     }
+
+    for(int i = 0; i < s.size(); i++)
+        cout << c[i];
 }
