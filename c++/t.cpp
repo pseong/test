e9a4e474cdca6bac635e660aa5746813026ac306
Guest <guest-8yoeyp@h29043.sgb.skir.kr>
2021-01-19 19:36:31 +0900

    commit

--- c++/t.cpp
+++ c++/t.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
-char m[26][26]{ 0 };
+bool m[51][51]{ 0 };
 
-int n = 0;
-int sizee = 0;
+int h = 0;
+int w = 0;
 
 void find(int i , int j ) {
-    if(i < 1 || j < 1 || i > n || j > n || m[i][j] == '0') return;
-    if(m[i][j] == '1') {
-        m[i][j] = '0';
-        sizee++;
-    }
+    if(i < 1 || j < 1 || i > h || j > w || m[i][j] == 0) return;
+    if(m[i][j] == 1) m[i][j] = 0;
+    find(i - 1, j - 1);
     find(i - 1, j);
-    find(i, j + 1);
-    find(i + 1, j);
+    find(i - 1, j + 1);
     find(i, j - 1);
+    find(i, j + 1);
+    find(i + 1, j - 1);
+    find(i + 1, j);
+    find(i + 1, j + 1);
 }
 
 int main() {
@@ -24,25 +25,26 @@ int main() {
     cin.tie(0); 
     cout.tie(0);
 
-    cin >> n;
+    while(1) {
+        cin >> w >> h;
+        if(w == 0 && h == 0) break;
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++)
-            m[i][j] = getchar();
-        char trash = getchar();
+        for(int i = 1; i <= h; i++)
+            for(int j = 1; j <= w; j++)
+                cin >> m[i][j];
+
+        int result = 0;
+        for(int i = 1; i <= h; i++)
+            for(int j = 1; j <= w; j++)
+                if(m[i][j] == 1) {
+                    find(i, j);
+                    result++;
+                }
+
+        cout << result << '\n';
+
+        for(int i = 1; i <= h; i++)
+            for(int j = 1; j <= w; j++)
+                m[i][j] = 0;
     }
-
-    vector<int> vec;
-    for(int i =1; i <= n; i++)
-        for(int j = 1; j <= n; j++)
-            if(m[i][j] == '1') {
-                find(i, j);
-                vec.push_back(sizee);
-                sizee = 0;
-            }
-
-    sort(vec.begin(), vec.end());
-    cout << vec.size() << '\n';
-    for(int i : vec)
-        cout << i << '\n';
 }
