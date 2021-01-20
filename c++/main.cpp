b6dc47081812ab79026ec7b59e54a63d11542e66
Guest <guest-edzs76@h29033.sgb.skir.kr>
2021-01-20 10:28:25 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,51 +2,44 @@
 
 using namespace std;
 
-int n, m;
-
-bool valid(int i, int j) {
-    if(i < 1 || j < 1 || i > n || j > m) return false;
-    else return true;
-}
-
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); 
     cout.tie(0);
 
-    int box[110][110] {0};
-    cin >> n >> m;
-
+    vector<char> vc;
     string s;
-    for(int i = 1; i <= n; i++) {
-        cin >> s;
-        
-        for(int j = 1; j <= m; j++) {
-            box[i][j] = s[j - 1] - '0';
+    cin >> s;
+    for(int i = 0; i < s.size(); i ++)
+        vc.push_back(s[i]);
+
+    int cur = vc.size();
+    int m;
+    cin >> m;
+    while(m--) {
+        char cmd;
+        cin >> cmd;
+        switch(cmd) {
+            case 'L':
+                cur -= 1;
+                if(cur < 0) cur = 0;
+                break;
+            case 'D':
+                cur++;
+                if(cur > vc.size()) cur = vc.size();
+                break;
+            case 'B':
+                if(cur > 0) {
+                    vc.erase(vc.begin() + cur - 1);
+                    cur--;
+                }
+                break;
+            case 'P':
+                char a;
+                cin >> a;
+                vc.insert(vc.begin() + cur, a);
+                cur++;
+                break;
         }
     }
-
-    int h[] {-1, 1, 0, 0};
-    int w[] {0, 0, -1, 1};
-
-    queue<tuple<int, int, int>> que;
-    int result = 1;
-
-    box[1][1] = 2; 
-    que.push({1, 1, 2});
-    while(!que.empty()) {
-        auto a = que.front(); que.pop();
-        int i = get<0>(a); int j = get<1>(a); int k = get<2>(a);
-        if(i == n && j == m) {
-            result = k;
-            break;
-        }
-        for(int p = 0; p < 4; p++) {
-            if(box[i + h[p]][j + w[p]] == 0 && valid(i + h[p], j + w[p])) {
-                box[i + h[p]][j + w[p]] = k + 1;
-                que.push({i + h[p], j + w[p], k + 1});
-            }
-        }
-    }
-    cout << result - 1;
 }
