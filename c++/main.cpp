e22de492f5fe5322802ae4bad32a787751f69566
Guest <guest-edzs76@h29033.sgb.skir.kr>
2021-01-20 10:04:19 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,50 +2,51 @@
 
 using namespace std;
 
+int n, m;
+
+bool valid(int i, int j) {
+    if(i < 1 || j < 1 || i > n || j > m) return false;
+    else return true;
+}
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); 
     cout.tie(0);
 
-    int t;
-    cin >> t;
+    int box[110][110] {0};
+    cin >> n >> m;
 
-    int adj[100010]{ 0 };
-    while(t--) {
-        int n;
-        cin >> n;
-        for(int i = 1; i <= n; i++) {
-            int a;
-            cin >> a;
-            adj[i] = a;
-        }
-
-        bool visited[100010]{ 0 };
-        short team[100010]{ 0 };
-
-        for(int i = 1; i <= n; i++) {
-            if (visited[i]) continue;
-            int go = i;
-            while(!visited[go]) {
-                visited[go] = 1;
-                go = adj[go];
-            }
-            if(team[go] == 0) {
-                while(!team[go]) {
-                    team[go] = 1;
-                    go = adj[go];
-                }
-            }
-            go = i;
-            while(!team[go]) {
-                team[go] = 2;
-                go = adj[go];
-            }
-        }
+    string s;
+    for(int i = 1; i <= n; i++) {
+        cin >> s;
         
-        int result = 0;
-        for(int i = 1; i <= n; i++)
-            if(team[i] == 2) result++;
-        cout << result << '\n';
+        for(int j = 1; j <= m; j++) {
+            box[i][j] = s[j - 1] - '0';
+        }
     }
+
+    int h[] {-1, 1, 0, 0};
+    int w[] {0, 0, -1, 1};
+
+    queue<tuple<int, int, int>> que;
+    int result = 1;
+
+    box[1][1] = 2; 
+    que.push({1, 1, 2});
+    while(!que.empty()) {
+        auto a = que.front(); que.pop();
+        int i = get<0>(a); int j = get<1>(a); int k = get<2>(a);
+        if(i == n && j == m) {
+            result = k;
+            break;
+        }
+        for(int p = 0; p < 4; p++) {
+            if(box[i + h[p]][j + w[p]] == 0 && valid(i + h[p], j + w[p])) {
+                box[i + h[p]][j + w[p]] = k + 1;
+                que.push({i + h[p], j + w[p], k + 1});
+            }
+        }
+    }
+    cout << result - 1;
 }
--- c++/t.cpp
+++ c++/t.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-short m, n;
+int m, n;
 
-bool valid(short i, short j) {
+bool valid(int i, int j) {
     if(i < 1 || j < 1 || i > n || j > m) return false;
     else return true;
 }
@@ -14,35 +14,39 @@ int main() {
     cin.tie(0); 
     cout.tie(0);
 
-    short box[1010][1010]{ 0 };
+    int box[1010][1010]{ 0 };
     cin >> m >> n;
 
-    queue<tuple<short, short, short>> que;
-    
-    for(int i = 1; i <= n; i++)
+    queue<tuple<int, int, int>> que;
+    for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= m; j++) {
             cin >> box[i][j];
             if (box[i][j] == 1) que.push({i, j, 1});
         }
-    
+    }
+     
+    int h[] {-1, 1, 0, 0};
+    int w[] {0, 0, -1, 1};
     while(!que.empty()) {
         auto a = que.front(); que.pop();
         int i = get<0>(a); int j = get<1>(a); int k = get<2>(a);
-        box[i][j] = k;
 
-        if(box[i - 1][j] == 0 && valid(i - 1, j)) que.push({i - 1, j, k + 1});
-        if(box[i + 1][j] == 0 && valid(i + 1, j)) que.push({i + 1, j, k + 1});
-        if(box[i][j - 1] == 0 && valid(i, j - 1)) que.push({i, j - 1, k + 1});
-        if(box[i][j + 1] == 0 && valid(i, j + 1)) que.push({i, j + 1, k + 1});
+        for(int p = 0; p < 4; p++) {
+            if(box[i + h[p]][j + w[p]] == 0 && valid(i + h[p], j + w[p])) {
+                box[i + h[p]][j + w[p]] = k + 1;
+                que.push({i + h[p], j + w[p], k + 1});
+            }
+        }
     }
 
-    short mx = 0;
+    int mx = 0;
     bool result = 1;
-    for(int i = 1; i <= n; i++)
+    for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= m; j++) {
             mx = max(mx, box[i][j]);
             if(box[i][j] == 0) result = 0;
         }
+    }
     if(result) cout << mx - 1;
     else cout << -1;
 }
