8db664dbe2e1f2a03f5f91b06c5fd7a8d348c789
Guest <guest-fhtgqo@h29038.sgb.skir.kr>
2021-01-21 14:35:34 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,68 +2,43 @@
 
 using namespace std;
 
-int cont[101][101]{ 0 };
-int m[101][101]{ 0 };
-int n{ 0 };
+int result = 0;
+vector<pair<int, int>> adj[100010];
 
-int ii[]{-1, 1, 0, 0};
-int jj[]{0, 0, -1, 1};
-
-int valid(int i, int j) {
-    if(i > n || j > n || i < 1 || j < 1) return false;
-    else return true;
-}
-
-void dfs(int i, int j, int id) {
-    cont[i][j] = id;
-    for(int q = 0; q < 4; q++)
-        if(cont[i + ii[q]][j + jj[q]] == 1) dfs(i + ii[q], j 
+void dfs(int n, int dia, int pre) {
+    result = max(result, dia);
+    for(auto a : adj[n]) {
+        if(a.first == pre) continue;
+        dfs(a.first, dia + a.second, n);
+    }
 }
 
 int main() {
-    ios::sync_with_stdio(0); 
-    cin.tie(0); 
-    cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    queue<tuple<int, int, int>> que;
-    cin >> n;
-
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> [i][j];
-            cont[i][j] = m[i][j];
-            if(m[i][j] == 1) que.push({i, j, 1});
+    int v;
+    cin >> v;
+    for(int i = 1; i <= v; i++) {
+        int n;
+        cin >> n;
+        while(1) {
+            int n2;
+            cin >> n2;
+            if(n2 == -1) break;
+            int n3;
+            cin >> n3;
+            adj[n].push_back({n2, n3});
+        }
+    }
+    int start = 0;
+    for(int i = 1; i <= v; i++) {
+        if(adj[i].size() == 1) {
+            start = i;
+            break;
         }
     }
 
-    int id = 2;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            if(cont[i][j] == 1) {
-                dfs(i, j, id);
-                id++;
-            }
-        }
-    }
-
-    int result = 0;
-    while(!que.empty()) {
-        if(result) break;
-        auto a = que.front(); que.pop();
-        int i = get<0>(a); int j = get<1>(a); int k =  get<2>(a);
-        for(int q = 0; q < 4; q++) {
-            if(valid(i + ii[q], j + jj[q])) {
-                if(m[i + ii[q]][j + jj[q]] == 0) {
-                    m[i + ii[q]][j +jj[q]] = k + 1;
-                    cont[i + ii[q]][j + jj[q]] = cont[i][j];
-                    que.push({i + ii[q], j + jj[q], k + 1});
-                } else {
-                    if(cont[i + ii[q]][j + jj[q]] != cont[i][j]) {
-                        result = m[i + ii[q]][j + jj[q]] + m[i][j] - 2;
-                        break;
-                    }
-                }
-            }
-        }
-    }
+    dfs(start, 0, 0);
+    cout << result;
 }
