567a9db71dbe6c00be9d73695f2658e52b7a4b9a
Guest <guest-mhrz6c@h29038.sgb.skir.kr>
2021-01-21 13:51:59 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,40 +2,68 @@
 
 using namespace std;
 
+int cont[101][101]{ 0 };
+int m[101][101]{ 0 };
+int n{ 0 };
+
+int ii[]{-1, 1, 0, 0};
+int jj[]{0, 0, -1, 1};
+
+int valid(int i, int j) {
+    if(i > n || j > n || i < 1 || j < 1) return false;
+    else return true;
+}
+
+void dfs(int i, int j, int id) {
+    cont[i][j] = id;
+    for(int q = 0; q < 4; q++)
+        if(cont[i + ii[q]][j + jj[q]] == 1) dfs(i + ii[q], j 
+}
+
 int main() {
     ios::sync_with_stdio(0); 
     cin.tie(0); 
     cout.tie(0);
 
-    list<char> vc;
-    string s;
-    cin >> s;
-    for(int i = 0; i < s.size(); i ++)
-        vc.push_back(s[i]);
+    queue<tuple<int, int, int>> que;
+    cin >> n;
 
-    auto it = vc.end();
-    int m;
-    cin >> m;
-    while(m--) {
-        char cmd;
-        cin >> cmd;
-        switch(cmd) {
-            case 'L':
-                if(it != vc.begin()) it--;
-                break;
-            case 'D':
-                if(it != vc.end()) it++;
-                break;
-            case 'B':
-                if(it != vc.end()) it = vc.erase(it);
-                break;
-            case 'P':
-                char a;
-                cin >> a;
-                vc.insert(it, a);
-                break;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cin >> [i][j];
+            cont[i][j] = m[i][j];
+            if(m[i][j] == 1) que.push({i, j, 1});
         }
     }
-    for(char c : vc)
-        cout << c;
+
+    int id = 2;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(cont[i][j] == 1) {
+                dfs(i, j, id);
+                id++;
+            }
+        }
+    }
+
+    int result = 0;
+    while(!que.empty()) {
+        if(result) break;
+        auto a = que.front(); que.pop();
+        int i = get<0>(a); int j = get<1>(a); int k =  get<2>(a);
+        for(int q = 0; q < 4; q++) {
+            if(valid(i + ii[q], j + jj[q])) {
+                if(m[i + ii[q]][j + jj[q]] == 0) {
+                    m[i + ii[q]][j +jj[q]] = k + 1;
+                    cont[i + ii[q]][j + jj[q]] = cont[i][j];
+                    que.push({i + ii[q], j + jj[q], k + 1});
+                } else {
+                    if(cont[i + ii[q]][j + jj[q]] != cont[i][j]) {
+                        result = m[i + ii[q]][j + jj[q]] + m[i][j] - 2;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
