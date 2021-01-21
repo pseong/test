0bf0f28b3b565ca50f36d3bb8dc36c5f52217ecc
Guest <guest-09o6xs@h29038.sgb.skir.kr>
2021-01-22 08:45:11 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -6,55 +6,34 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    bool prime[10000]{ 0 };
-    for(int i = 3; i < 10000; i += 2)
-        prime[i] = 1;
-
-    vector<int> vprime;
-    pair<int, int> result[10000];
-    prime[1] = 0;
-    for(int i = 3; i < 10000; i += 2) {
-        if(prime[i] == 0) continue;
-        vprime.push_back(i);
-        for(int j = i * 3; j < 10000; j += (i * 2))
-            prime[j] = 0;
-        result[i] = {0, 0};
+    long long  mx = 0;
+    long long a[10001]{ 0 };
+    int k, n;
+    cin >> k >> n;
+    for(int i = 1; i <= k; i++) {
+        cin >> a[i];
+        mx = max(mx, a[i]);
     }
 
 
+    long long result = 0;
+    long long  left = 1;
+    long long  right = mx;
+    while(left <= right) {
+        long long  mid = (left + right) / 2;
 
-    int prm_size = vprime.size();
-
-    while(1) {
-        //int n;
-        //cin >> n;
-        for(int n = 6; n < 50; n += 2) {
-            if(n == 0) break;
-
-            if(result[n].first) {
-                cout << n << " = " << result[n].first << 
-                        " + " << result[n].second << '\n';
-                continue;
-            }
-            int left = 0;
-            int right = prm_size - 1;
-
-            while(1) {
-                int sum = vprime[left] + vprime[right];
-                result[sum].first = vprime[left];
-                result[sum].second = vprime[right];
-                if(sum > n) right--;
-                else if(sum < n) left++;
-                else {
-                    cout << n << " = " << vprime[left] << 
-                        " + " << vprime[right] << '\n';
-                    break;
-                }
-                if(left > right) {
-                    cout << "Goldbach's conjecture is wrong.\n";
-                    break;
-                }
-            }
+        int made = 0;
+        for(int i = 1; i <= k; i++) {
+            made += a[i] / mid;
+        }
+        
+        if(made < n) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+            result = max(result, mid);
         }
     }
+
+    cout << result;
 }
