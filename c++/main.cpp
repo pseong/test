7792fca6c19d6ff11ad9828a6b8b3cadba7c2d9c
Guest <guest-efruuc@h29038.sgb.skir.kr>
2021-01-23 09:22:42 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -8,33 +8,41 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a[500001]{ 0 };
-    int n;
-    cin >> n;
+    int ax, ay, bx, by, cx, cy, dx, dy;
+    cin >> ax >> ay >> bx >> by >> cx >> cy >> dx >> dy;
 
-    for(int i = 1; i <= n; i++) cin >> a[i];
+    double lo = 0;
+    double hi = 0;
 
-    sort(a + 1, a + 1 + n);
+    for(int i = 0; i < 100; i++) {
+        double mx1 = (2*ax + bx) / 3;
+        double my1 = (2*ay + by) / 3;
 
-    int x[500001]{ 0 };
-    int m;
-    cin >> m;
+        double mx2 = (ax + 2*bx) / 3;
+        double my2 = (ay + 2*by) / 3;
 
-    for(int i = 1; i <= m; i++) {
-        int left = 1;
-        int right = n;
-        bool find = 0;
-        while(left <= right) {
-            int mid = (left + right) >> 1;
-            if (x[i] > x[mid]) {
-                left = mid + 1;
-            } else if (x[i] < x[mid]) {
-                right = mid - 1;
-            } else {
-                find = 1;
-                break;
-            }
+        double gx1 = (2*cx + dx) / 3;
+        double gy1 = (2*cy + dy) / 3;
+
+        double gx2 = (cx + 2*dx) / 3;
+        double gy2 = (cy + 2*dy) / 3;
+
+        lo = pow(mx1 - gx1, 2) + pow(my1 - gy1, 2);
+        hi = pow(mx2 - gx2, 2) + pow(my2 - gy2, 2);
+
+        if(lo <= hi) {
+            bx = mx2;
+            by = my2;
+            dx = gx2;
+            dy = gy2;
         }
-        cout << (find ? 1 : 0) << ' ';
+        else {
+            ax = mx1;
+            ay = my1;
+            cx = gx1;
+            cy = gx1;
+        }
     }
+
+    cout << (lo + hi) / 2;
 }
