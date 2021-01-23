c19b75deddb6b9c8d733f92bff292e02d46f9134
Guest <guest-wfxd6d@h29038.sgb.skir.kr>
2021-01-23 14:38:26 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,54 +2,46 @@
 
 using namespace std;
 
+int mp[2200][2200]{ 0 };
+int result[3]{ 0 };
+
+bool valid(int i, int j, int i2, int j2) {
+    int pre = mp[i][j];
+    for(int n = i; n <= i2; n++) {
+        for(int m = j; m <= j2; j++) {
+            if(pre != mp[n][m]) {
+                return false;
+            }
+            pre = mp[n][m];
+        }
+    }
+    result[++pre]++;
+    return true;
+}
+
+int partition(int i, int j, int i2, int j2) {
+    if(valid(i, j, i2, j2)) return;
+    int third = (i2 - i + 1) / 3;
+    for(int n = i; i < i2; i += third) {
+        for(int m = j; j < j2; j += third) {
+            partition(n, m, n + third - 1, m + third - 1);
+        }
+    }
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    vector<int> a;
-    vector<int> b;
-
-    int n, m;
-    cin >> n >> m;
-
-    int d;
+    int n;
+    cin >> n;
     for(int i = 1; i <= n; i++) {
-        cin >> d;
-        a.push_back(d);
-    }
-    for(int i = 1; i <= m; i++) {
-        cin >> d;
-        b.push_back(d);
-    }
-
-    vector<int> result;
-
-    int size_a = a.size();
-    int size_b = b.size();
-
-    int i = 0;
-    int j = 0;
-
-    while(1) {
-        if(a[i] >= b[j]) {
-            result.push_back(b[j]);
-            ++j;
+        for(int j = 1; j <= n; j++) {
+            cin >> mp[i][j];
         }
-        else {
-            result.push_back(a[i]);
-            ++i;
-        }
-        if(i == size_a || j == size_b) break;
     }
-    if(j == size_b) {
-        swap(a, b);
-        swap(i, j);
-    }
-    while(i != size_a) {
-        result.push_back(a[i]);
-        i++;
-    }
-    for(int re : result) {
-        cout << re << ' ';
+    partition(1, 1, n, n);
+    for(int i = 0; i < 2; i++) {
+        cout << result[i] << '\n';
     }
 }
