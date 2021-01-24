a9e94f187e070579b6e6efb966594f0f28ba642e
Guest <guest-qoa79l@h29038.sgb.skir.kr>
2021-01-24 09:57:27 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-bool mp[65][65]{ 0 };
+int mp[65][65]{ 0 };
 int n;
 
 bool valid(int i, int j, int i2, int j2) {
     bool pre = mp[i][j];
-    for(int i  = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
+    for(int i  = 1; i <= i2; i++) {
+        for(int j = 1; j <= j2; j++) {
             if(mp[i][j] != pre) return false;
         }
     }
@@ -19,7 +19,7 @@ void partition(int i, int j, int i2, int j2) {
     cout << '(';
     if(valid(i, j, i2, j2)) cout << mp[i][j];
     else {
-        int gap = (i + j - 1) / 2;
+        int gap = (i + i2 - 1) / 2;
         partition(i, j, i+gap-1, j+gap-1);
         partition(i, j+gap, i+gap-1, j+2*gap-1);
         partition(i+gap, j, i+2*gap-1, j+gap-1);
@@ -38,7 +38,7 @@ int main() {
     for(int i  = 1; i <= n; i++) {
         cin >> s;
         for(int j = 1; j <= n; j++) {
-            mp[i][j] = s[j - 1];
+            mp[i][j] = s[j - 1] - '0';
         }
     }
 
