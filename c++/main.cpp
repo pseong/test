54209ccf5a63947d8c046f4743494d204aeb39e1
Guest <guest-vxhwvy@h29038.sgb.skir.kr>
2021-01-24 09:32:32 +0900

    commit:

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,46 +2,35 @@
 
 using namespace std;
 
-int mp[2200][2200]{ 0 };
-int result[3]{ 0 };
+bool mp[65][65]{ 0 };
+int n;
 
 bool valid(int i, int j, int i2, int j2) {
-    int pre = mp[i][j];
-    for(int n = i; n <= i2; n++) {
-        for(int m = j; m <= j2; j++) {
-            if(pre != mp[n][m]) {
-                return false;
-            }
-            pre = mp[n][m];
+    bool pre = mp[i][j];
+    for(int i  = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cin >> mp[i][j];
         }
     }
-    result[++pre]++;
-    return true;
 }
 
-int partition(int i, int j, int i2, int j2) {
-    if(valid(i, j, i2, j2)) return;
-    int third = (i2 - i + 1) / 3;
-    for(int n = i; i < i2; i += third) {
-        for(int m = j; j < j2; j += third) {
-            partition(n, m, n + third - 1, m + third - 1);
-        }
-    }
+void partition(int i, int j, int i2, int j2) {
+
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
     cin >> n;
-    for(int i = 1; i <= n; i++) {
+
+    string s;
+    for(int i  = 1; i <= n; i++) {
+        cin >> s;
         for(int j = 1; j <= n; j++) {
-            cin >> mp[i][j];
+            mp[i][j] = s[j - 1];
         }
     }
+
     partition(1, 1, n, n);
-    for(int i = 0; i < 2; i++) {
-        cout << result[i] << '\n';
-    }
 }
