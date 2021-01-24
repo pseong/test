e26259e45de76c656777604b1bec7e84dc13c9ea
Guest <guest-vxhwvy@h29038.sgb.skir.kr>
2021-01-24 09:44:07 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -9,13 +9,23 @@ bool valid(int i, int j, int i2, int j2) {
     bool pre = mp[i][j];
     for(int i  = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            cin >> mp[i][j];
+            if(mp[i][j] != pre) return false;
         }
     }
+    return true;
 }
 
 void partition(int i, int j, int i2, int j2) {
-
+    cout << '(';
+    if(valid(i, j, i2, j2)) cout << mp[i][j];
+    else {
+        int gap = (i + j - 1) / 2;
+        partition(i, j, i+gap-1, j+gap-1);
+        partition(i, j+gap, i+gap-1, j+2*gap-1);
+        partition(i+gap, j, i+2*gap-1, j+gap-1);
+        partition(i+gap, j+gap, i+2*gap-1, j+2*gap-1);
+    }
+    cout << ')';
 }
 
 int main() {
