5247992d392318bc348b698cd3deb41a3c1629a7
Guest <guest-qoa79l@h29038.sgb.skir.kr>
2021-01-24 14:21:26 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,48 +2,33 @@
 
 using namespace std;
 
-int a[501]{ 0 };
-int tree[20000]{ 0 };
-int tree_size[20000]{ 0 };
+int a[500001]{ 0 };
+int sorted[500001]{ 0 };
+int cnt = 0;
 int ans = 0;
-int n;
 
-int init(int start, int end, int node) {
-    if(start == end) {
-        tree_size[node] = 1;
-        return tree[node] = a[start];
+void merge(int m, int mid, int n) {
+    int i = m;
+    int j = mid + 1;
+    int k = m;
+
+    while(i <= mid && j <= n) {
+        if(a[i] <= a[j]) {
+
+        }
     }
-    int mid = (start + end) / 2;
-    int a = init(start, mid, node * 2);
-    tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
-    tree_size[node] = tree_size[node * 2] + tree_size[node * 2 + 1];
-    return tree[node];
 }
 
-void bubble(int node) {
-    if(node > n * 4 || !tree_size[node]) return;
-    if(tree[node * 2] > tree[node * 2 + 1]) {
-        ans += (tree_size[node * 2] + tree_size[node * 2 + 1]);
-    } 
-    else {
-        bubble(node * 2);
-        bubble(node * 2 + 1);
+void mergeSort(int m, int n) {
+    if(m < n) {
+        int mid = (m + n) / 2;
+        mergeSort(m, mid);
+        mergeSort(mid + 1, n);
+        merge(m, mid, n);
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    cin >> n;
-    for(int i = 1; i <= n; i++) {
-        cin >> a[i];
-    }
-
-    init(1, n, 1);
-    for(int i = 1; i <= n * 4; i++) {
-        cout << tree[i] << ' ';
-    }
-
-    cout << ans;
 }
